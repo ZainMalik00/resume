4236e76b415cb38ba828d8ab2e7191c13ef7d52c
ZainMalik00 <zmmalik00@gmail.com>
2025-12-17 13:46:54 -0500

    Added Dropdown Menu On Smaller Screens and Cleaned Up Borders

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { AboutMeSection } from './pages/about-me-section/about-me-section';
 import { ExperienceSection } from './pages/experience-section/experience-section';
 import { ContactSection } from './pages/contact-section/contact-section';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,23 @@ import { ContactSection } from './pages/contact-section/contact-section';
     RouterModule,
     NzLayoutModule,
     NzMenuModule,
+    NzDropDownModule,
     NzPageHeaderModule,
     AboutMeSection,
     ExperienceSection,
-    ContactSection
-  ],
+    ContactSection,
+    NzIconModule
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+
+  menuType: 'expanded' | 'dropdown' = window.innerWidth <= 768 ? 'dropdown' : 'expanded';
+
+  @HostListener('window:resize')
+  onResize() {
+    this.menuType = window.innerWidth <= 768 ? 'dropdown' : 'expanded';
+  }
+
+}
